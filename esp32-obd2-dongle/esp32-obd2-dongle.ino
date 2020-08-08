d82c23e88a4ee5265d4615ea2c92650ec98d3370
Ganeshkumar Swaminathan <wd973684@varian.com>
2020-08-08 21:47:38 +0200

    - Update module name in version

--- esp32-obd2-dongle/esp32-obd2-dongle.ino
+++ esp32-obd2-dongle/esp32-obd2-dongle.ino
@@ -16,7 +16,7 @@ void setup()
 {
     Serial.begin(460800);
     Serial.setRxBufferSize(4096);
-    String ver = "\r\n\r\n\r\nOBDII USB/Wifi Dongle v" xstr(MAJOR_VERSION) "." xstr(MINOR_VERSION) "." xstr(SUB_VERSION) " <" SW_VERSION "> <";
+    String ver = "\r\n\r\n\r\nOBDII USB/BLE Dongle v" xstr(MAJOR_VERSION) "." xstr(MINOR_VERSION) "." xstr(SUB_VERSION) " <" SW_VERSION "> <";
     ver = ver + ESP.getSdkVersion() + ">";
 
     Serial.println(ver);
